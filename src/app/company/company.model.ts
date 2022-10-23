e6b8db03073932804dcb9e60abc3e80b16959e04
Nikunj patel <nikunj199842@gmail.com>
2022-10-23 11:26:35 +0530

    feat:folder-structure

---
+++ src/app/company/company.model.ts
@@ -0,0 +1,8 @@
+export class Company{
+    id?: any;
+    companyname?: string;
+    companydescription?: string;
+    companytags?: string;
+    companylogo?: string
+
+}
---
+++ src/app/company/company.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CompanyRoutingModule } from './company-routing.module';
+import { CompanyComponent } from './company.component';
+import { FormComponent } from './form/form.component';
+import { CompanyFormComponent } from './company-form/company-form.component';
+import { CompanyListComponent } from './company-list/company-list.component';
+import { CompanyPipe } from './pipe/company.pipe';
+
+
+@NgModule({
+  declarations: [
+    CompanyComponent,
+    FormComponent,
+    CompanyFormComponent,
+    CompanyListComponent,
+    CompanyPipe
+  ],
+  imports: [
+    CommonModule,
+    CompanyRoutingModule
+  ]
+})
+export class CompanyModule { }
