8ec3800767525fdbd9de4c432cce193bcde93166
Nikunj patel <nikunj199842@gmail.com>
2022-10-24 10:11:25 +0530

    feat:routing

--- src/app/company/company-routing.module.ts
+++ src/app/company/company-routing.module.ts
@@ -1,8 +1,17 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CompanyFormComponent } from './company-form/company-form.component';
 import { CompanyComponent } from './company.component';
 
-const routes: Routes = [{ path: '', component: CompanyComponent }];
+const routes: Routes = [{ path: '', component: CompanyComponent ,
+
+children:[{
+  path:'company-form',
+  component:CompanyFormComponent
+}
+]
+
+}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
